d12cfb187d726c5af7596cc8bd0d08b3de514b1a
Thiago de Santana <168003660+thiagoandradez@users.noreply.github.com>
2025-01-07 13:51:46 -0300

    end projects

--- thiagoandradez/scripts/script.js
+++ thiagoandradez/scripts/script.js
@@ -6,3 +6,9 @@ button.addEventListener('click', function() {
     mobileNavbar.classList.toggle('active');
 
 });
+
+
+window.addEventListener('scroll', function() {
+    if(this.window.pageYOffset > 0) return navbar.classList.add('active')
+    return navbar.classList.remove('active');
+})
